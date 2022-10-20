f87ca88705aa26a339540595f20dac47309550af
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-20 05:21:09 -0400

    Add head line to the console

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 {
     static void Main(string[] args)
     {
-
+        // Print a header line
+        Console.WriteLine(
+            "\nJason D'Oyley - Week 3 GP Passing Arrays and Lists\n"
+        );
     }
 }
